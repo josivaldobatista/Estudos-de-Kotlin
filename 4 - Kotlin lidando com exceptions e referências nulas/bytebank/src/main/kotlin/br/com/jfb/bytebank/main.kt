fec64d33f032f6208a05c323dfdeeb45a6afdfcf
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-07-08 14:26:53 -0300

    Utilizando as expressões

--- 4 - Kotlin lidando com exceptions e referências nulas/bytebank/src/main/kotlin/br/com/jfb/bytebank/main.kt
+++ 4 - Kotlin lidando com exceptions e referências nulas/bytebank/src/main/kotlin/br/com/jfb/bytebank/main.kt
@@ -2,9 +2,33 @@ import br.com.jfb.bytebank.models.Endereco
 import br.com.jfb.bytebank.test.testaAny
 import java.lang.ArithmeticException
 import java.lang.ClassCastException
+import java.lang.NumberFormatException
 
 fun main() {
   println("início main")
+
+  val entrada: String = "1.0"
+  val valorRecebido: Double? = try {
+    entrada.toDouble()
+  } catch (e: NumberFormatException) {
+    println("Erro na conversão")
+    e.printStackTrace()
+    null
+  }
+  val valorComTaxa: Double = if (valorRecebido != null) { // ?
+    valorRecebido + 0.1
+  } else {
+    // Podemos colocar um valor padrão em vez de null como está abaixo
+      // Veja o exemplo acima
+    0.0
+    //  Em vez null
+  }
+
+  if (valorComTaxa != null) {
+    println("Valor recebido: $valorComTaxa")
+  } else {
+    println("Valor invalido: $valorComTaxa")
+  }
   try {
     10 / 0
   } catch (e: ArithmeticException) {
