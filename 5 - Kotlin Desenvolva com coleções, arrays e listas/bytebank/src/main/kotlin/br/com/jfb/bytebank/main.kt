5aec6f85e160e21e5d830e488520a5a184798df2
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-07-09 14:31:07 -0300

    Funções de agregação: Aula final do curso

--- 5 - Kotlin Desenvolva com coleções, arrays e listas/bytebank/src/main/kotlin/br/com/jfb/bytebank/main.kt
+++ 5 - Kotlin Desenvolva com coleções, arrays e listas/bytebank/src/main/kotlin/br/com/jfb/bytebank/main.kt
@@ -1,8 +1,54 @@
 package br.com.jfb.bytebank
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 fun main() {
 
-  
+  val nomes = stringArrayOf("Valdo Batista", "Josiane Silva", "Bob Brown")
+  println("Lista de nomes: ${nomes.contentToString()}")
+
+  /**
+   * Como o BigDecimal não tem a facilidade que os outros primitivos tem
+   * que é ter uma forma de criar lista de tipois temos que fazelo "manualmente".
+   * Veja abaixo como fazer isso:
+   * */
+  val salarios = bigDecimalArrayOf("1499.90", "2500.00", "5000.00", "10000.00")
+  // Array<BigDecimal>(5) { BigDecimal.ZERO }//<- Temos que iniciar o array com algum elemento "0"
+  // salarios[0] = "1500.00".toBigDecimal() <- Para não ter problema de arredondamento, usamos o String para o valor
+  // salarios[1] = "2500.00".toBigDecimal()
+  println(salarios.contentToString())
+
+  val aumento = "1.1".toBigDecimal()
+  val salariosComAumento: Array<BigDecimal> = salarios
+    .map { salario ->
+      if (salario < "5000.00".toBigDecimal()) {
+        salario + "500.00".toBigDecimal()
+      } else {
+        (salario * aumento).setScale(2, RoundingMode.UP)
+      }
+    }.toTypedArray()
+  println(salariosComAumento.filter { it < "5000.00".toBigDecimal() })
+
+} // <- Fim do main()
+
+/**
+ * Perceba que fazer isso todas as vezes é um pouco trabalhoso e como não temos
+ * a facilidade e usar os tipos primitivos, temos que usar uma forma um pouco mais
+ * "dificil" criando uma função para tal
+ * */
+fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> { //<- Os vararg serve para criar um conjunto de algum tipo
+  // Podemos ter 1 elemento ou 20 elementos dentro do vararg
+  return Array<BigDecimal>(valores.size) { i ->
+    valores[i].toBigDecimal()
+  }
+}
+
+// Apenas mais um teste
+fun stringArrayOf(vararg nomes: String): Array<String> {
+  return Array<String>(nomes.size) { i ->
+    nomes[i]
+  }
 }
 
 
--- 5 - Kotlin Desenvolva com coleções, arrays e listas/bytebank/src/main/kotlin/br/com/jfb/bytebank/test/arrays/testeAgregacao.kt
+++ 5 - Kotlin Desenvolva com coleções, arrays e listas/bytebank/src/main/kotlin/br/com/jfb/bytebank/test/arrays/testeAgregacao.kt
@@ -42,4 +42,43 @@ fun testeAgregacao() {
       "Está dentro de a até z"
     } else "Não está dentro de a até z"
   )
+
+  // TESTE DE AGREGAÇÃO VIDEO DOIS ALURA
+  /**
+   * A operação de verificar um valor maximo dentro de um array
+   * é uma operação muito recorrente que foi criado um forma mais simples
+   * que é usando o operador max(), porém foi deprecated :(. Mas podemos usar o
+   * "maxOrNull() no lugar que aparentemente tem o mesmo efeito"
+   * DOCUMENTAÇÃO: "Returns the largest element or null if there are no elements."
+   * */
+  val idades: IntArray = intArrayOf(10, 12, 18, 33, 40, 67)
+  val maiorIdade = idades.maxOrNull()
+  val idadeMinima = idades.minOrNull()
+  println("Idade Maxima: $maiorIdade")
+  println("Idade Minima: $idadeMinima")
+
+  // Verificando a média com average
+  val mediaIdades = idades.average()
+  println("Média das idades: $mediaIdades")
+
+  // Verifica uma condição com all
+  val maiorDeIdade = idades.all { it >= 18 }
+  if (maiorDeIdade) {
+    println("Todos são maiores de idade")
+  } else
+    println("Nem todos são maiores de idade")
+
+  // Verificando se pelo menos um satisfaz a condição com o any
+  val temPeloMenosUmMaiorDeIdade = idades.any { it >= 18 }
+  if (temPeloMenosUmMaiorDeIdade) {
+    println("Tem pelo menos um maior de idade")
+  }
+
+  // Filtra uma lista com uma condição pre determinada
+  val todosValoresMaiorQue18 = idades.filter { it >= 18 }
+  println("Maiores que 18: $todosValoresMaiorQue18")
+
+  // Encontra na lista um valor "find{}"
+  val idadeIgualA18 = idades.find { it == 18 }
+  println("Idade igual a $idadeIgualA18")
 }
